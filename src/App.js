e7b92519cbb095259149481ad18d74fb2cca65d8
Ritik <ritik123453@gmail.com>
2022-02-27 12:41:52 +0530

    Updated the error

--- src/App.js
+++ src/App.js
@@ -6,24 +6,23 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link
 } from "react-router-dom";
 
 export default class App extends Component {
-
+  pagesize=6
   render() {
     return (
       <Router>
         <div>
           <Navbar/>
           <Routes>
-            <Route path="/" element={<News pagesize={6} country='in' category="general"/>} />
-            <Route path="business" element={<News pagesize={6} country='in' category="business"/>} />
-            <Route path="entertainment" element={<News pagesize={6} country='in' category="entertainment"/>} />
-            <Route path="health" element={<News pagesize={6} country='in' category="health"/>} />
-            <Route path="science" element={<News pagesize={6} country='in' category="science"/>} />
-            <Route path="sports" element={<News pagesize={6} country='in' category="sports"/>} />
-            <Route path="technology" element={<News pagesize={6} country='in' category="technology"/>} />
+            <Route path="/" element={<News pagesize={this.pagesize} country='in' category="general"/>} />
+            <Route path="business" element={<News pagesize={this.pagesize} country='in' category="business"/>} />
+            <Route path="entertainment" element={<News pagesize={this.pagesize} country='in' category="entertainment"/>} />
+            <Route path="health" element={<News pagesize={this.pagesize} country='in' category="health"/>} />
+            <Route path="science" element={<News pagesize={this.pagesize} country='in' category="science"/>} />
+            <Route path="sports" element={<News pagesize={this.pagesize} country='in' category="sports"/>} />
+            <Route path="technology" element={<News pagesize={this.pagesize} country='in' category="technology"/>} />
           </Routes>
         </div>
       </Router>
